c8134a5af61f74624c0c11f6ba0732ca2f05773e
agimrof <agimjean@gmail.com>
2019-06-14 07:25:20 -0300

    Inicio do estudo

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+	define('DB_HOSTNAME', '192.168.56.101');
+	define('DB_USERNAME', 'jean');
+	define('DB_PASSWORD', 'jean');
+	define('DB_DATABASE', 'bancoteste');
+	//define('DB_CHARSERT', 'utf8');
+?>
